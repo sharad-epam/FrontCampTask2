48e4831a947e2e9bae7a091510a3787964df440e
Sharad Pawar <Sharad_Pawar@epam.com>
2019-04-05 16:47:00 +0530

    Changes for comments

---
+++ js/custom-comp.js
@@ -0,0 +1,92 @@
+class ResultInfo extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const wrapper = document.createElement("div");
+    wrapper.setAttribute("class", "container");
+
+    const author = document.createElement("h1");
+    author.setAttribute("class", "author");
+    author.textContent = this.getAttribute("author");
+
+    const title = document.createElement("div");
+    title.setAttribute("class", "title");
+    title.textContent = this.getAttribute("title");
+
+    const desc = document.createElement("div");
+    desc.setAttribute("class", "desc");
+    desc.textContent = this.getAttribute("desc");
+
+    const publish = document.createElement("div");
+    publish.setAttribute("class", "publish");
+    publish.textContent = this.getAttribute("publish");
+
+    const img = document.createElement("div");
+    img.setAttribute("class", "img");
+
+    let imgUrl;
+    this.hasAttribute("img") ? (imgUrl = this.getAttribute("img")) : "No Image";
+
+    let pic = document.createElement("img");
+    publish.setAttribute("class", "pic");
+    pic.src = imgUrl;
+    img.appendChild(pic);
+
+    const style = document.createElement("style");
+    console.log(style.isConnected);
+    style.innerHTML = `
+      .news {
+        display: flex;
+        flex-direction: column;
+      }
+      .author {
+        text-align: center;
+        border-bottom: 5px solid #dfe6e9;
+      }
+      .pic {
+        width: 250px;
+        height: 200px;
+        border-radius: 6%;
+        margin: 10px;
+      }
+      .title {
+        font-size: 20px;
+        color: #636e72;
+        border-bottom: 1px solid;
+        padding: 10px;
+      }
+      .desc {
+        padding: 10px;
+        color: #2d3436;
+        font-size: 20px;
+      }
+      .publish {
+        padding: 10px;
+        font-size: 15px;
+        border-bottom: 1px solid;
+        width: 8%;
+        color: #b2bec3;
+      }
+      @media only screen and (max-width: 500px) {
+        .publish {
+          width: 50%;
+        }
+      }
+      
+    `;
+
+    let news = document.createElement("li");
+    shadow.appendChild(style);
+    console.log(style.isConnected);
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(news);
+    news.appendChild(author);
+    news.appendChild(title);
+    news.appendChild(desc);
+    news.appendChild(publish);
+    news.appendChild(img);
+    news.appendChild(pic);
+  }
+}
+
+customElements.define("result-info", ResultInfo, { extends: "ul" });
--- js/index.js
+++ js/index.js
@@ -1,30 +1,24 @@
 let resultHtml = "";
-
+const getResults = (
+  { author, title, description, publishedAt, url, urlToImage },
+  index
+) => {
+  const item = index === 0 ? `<result-info ${author} >` : "";
+  resultHtml += `<div><result-info ${item} ${title} ${description} ${publishedAt} ${urlToImage}></result-info></div>`;
+};
 class fetchItems {
   fetchingNews = async url => {
     try {
       const res = await fetch(url);
       let data = await res.json();
       let newsId = document.getElementById("news");
-      data = data.articles.map((item, index) =>
-        returnUI.getResults(item, index)
-      );
-      newsId.insertAdjacentHTML("afterbegin", resultHtml);
+      data = data.articles.map((item, index) => getResults(item, index));
+      newsId.insertAdjacentHTML("afterend", resultHtml);
     } catch (err) {
       console.error(err);
     }
   };
 }
 
-class returnHtml extends fetchItems {
-  getResults = (
-    { author, title, description, publishedAt, url, urlToImage },
-    index
-  ) => {
-    const item = index === 0 ? `<h1 class="author">${author}</h1>` : "";
-    resultHtml += `${item}<div class="title">${title}</div><div class="desc">${description}</div><div class="publish">${publishedAt}</div><div class="img"><a href="${url}" target="_blank"><img class="pic" src=${urlToImage} /></a></div>`;
-  };
-}
 const result = new fetchItems();
-const returnUI = new returnHtml();
 export default result;
